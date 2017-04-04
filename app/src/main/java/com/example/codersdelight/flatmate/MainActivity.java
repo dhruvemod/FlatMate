e5edef2e232a6807a19f45884323c891de14c053
dhruvemod <dhruve.moudgildm@gmail.com>
2017-04-04 14:50:19 +0530

    Authentication, database added

--- app/src/main/java/com/example/codersdelight/flatmate/MainActivity.java
+++ app/src/main/java/com/example/codersdelight/flatmate/MainActivity.java
@@ -1,25 +1,42 @@
 package com.example.codersdelight.flatmate;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference firebaseDatabaseReference;
+    public static final String ANONYMOUS="anonymous";
     public static final int MAX_MSG_LEN_LIM=1000;
     public static final int RC_SIGN_IN=1;
     private ListView mMessageListView;
@@ -27,18 +44,25 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar ProgressBar;
     private EditText MessageEditText;
     private Button SendButton;
+    private String mUsername;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private ChildEventListener mChildEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUsername=ANONYMOUS;
         ProgressBar= (android.widget.ProgressBar) findViewById(R.id.progressBar);
         MessageEditText= (EditText) findViewById(R.id.messageEditText);
         SendButton= (Button) findViewById(R.id.sendButton);
         mMessageListView= (ListView) findViewById(R.id.messageListView);
         firebaseDatabase=FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabaseReference=firebaseDatabase.getReference().child("message");
         //initialising the message list view
-        List<chatMessages> chatMessages=new ArrayList<>();
+        final List<chatMessages> chatMessages=new ArrayList<>();
         arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,chatMessages);
         mMessageListView.setAdapter(arrayAdapter);
         ProgressBar.setVisibility(ProgressBar.INVISIBLE);
@@ -62,6 +86,102 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+MessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_MSG_LEN_LIM)});
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chatMessages chatMessages1=new chatMessages(mUsername,MessageEditText.getText().toString());
+                firebaseDatabaseReference.push().setValue(chatMessages1);
+                MessageEditText.setText(" ");
 
+            }
+        });
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    // User is signed out
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+
+                                            new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+
+                }
+            }
+        };
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+
+        }
+    }
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+        attachDatabaseReadListener();
+    }
+
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
+
+        detachDatabaseReadListener();
+    }
+    private void attachDatabaseReadListener() {
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    chatMessages chatMessage = dataSnapshot.getValue(chatMessages.class);
+
+                }
+
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+                public void onChildRemoved(DataSnapshot dataSnapshot) {}
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+                public void onCancelled(DatabaseError databaseError) {}
+            };
+            firebaseDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+    }
+
+    private void detachDatabaseReadListener() {
+        if (mChildEventListener != null) {
+            firebaseDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+
 }
