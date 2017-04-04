09107a38c40f73069c6558d64f3b36537ae8e77c
dhruvemod <dhruve.moudgildm@gmail.com>
2017-04-04 13:10:09 +0530

    Added the chatMessage contructor class

---
+++ app/src/main/java/com/example/codersdelight/flatmate/chatMessages.java
@@ -0,0 +1,29 @@
+package com.example.codersdelight.flatmate;
+
+/**
+ * Created by codersdelight on 4/4/17.
+ */
+
+public class chatMessages {
+    private String name;
+    private String message;
+public chatMessages(){
+}
+public chatMessages(String name,String message){
+    this.name=name;
+    this.message=message;
+}
+public String getName(){
+    return name;
+}
+public void setName(String name){
+    this.name=name;
+}
+public String getMessage(){
+    return message;
+}
+public void setMessage(String message){
+    this.message=message;
+}
+}
+
